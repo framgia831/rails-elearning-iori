3f5046b36915155e2d11d93d49751b31ee8fff77
iori19960828 <41966008+iori19960828@users.noreply.github.com>
2018-08-07 12:46:14 +0800

    All user list (#7)
    * All user list
    
    * All user list
    
    * All user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	before_action :correct_user, only: [:edit, :update]
+	before_action :require_login, except: [:new, :create]
+
+
 	def new
 		@user = User.new
 	end
@@ -44,10 +48,25 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def index
+		@users = User.paginate(page: params[:page], per_page: 10)
+	end
+
 
 	private 
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
+
+		def correct_user
+			user = User.find(params[:id])
+			unless current_user == user
+				flash[:danger] = "Error."
+				redirect_to root_url
+				
+			end
+		end
+
+
 
 end
