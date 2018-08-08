16cc40e54d0458d8d50033a9c3d924fae819085e
Iori Mukoyama <iori19960828@IorinoMacBook-Pro.local>
2018-08-08 12:33:23 +0800

    Add category page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+	validates :title, presence: true, 
+					  length:{ maximum: 50}
+
+	validates :description, presence: true, 
+					 		length:{ maximum: 150}
 end
